7b2b2db56ad6a35643b38708b9c9f024fbe0533c
maar34 <mar@headbrothers.com>
2024-11-08 12:16:02 +0100

    Test

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,22 @@
-// vite.config.js
-
 import { defineConfig } from 'vite'
 import restart from 'vite-plugin-restart'
 
 export default defineConfig({
-    base: process.env.VITE_BASE_URL, // Set base from the environment variable
+    base: '/', // Set to '/' to match the deployment root
     root: '.', // Root directory where index.html is located
     publicDir: 'public', // Serve static assets from 'public' folder
     server: {
-        host: true, // Make server accessible over the local network
-        open: true, // Automatically open the app in the browser on server start
+        host: true, 
+        open: true,
     },
     build: {
-        outDir: 'dist', // Output directory for the build
-        emptyOutDir: true, // Empty the output directory before building
-        sourcemap: true, // Generate sourcemaps for debugging
+        outDir: 'dist',
+        emptyOutDir: true,
+        sourcemap: true,
     },
     plugins: [
         restart({
-            restart: ['public/**'], // Restart server on changes in the public folder
+            restart: ['public/**'],
         }),
     ],
 })
